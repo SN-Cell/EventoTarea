a1e8f4cde434cd5d90927eb8dabea862f3d7652f
Francisco Sanhueza <francisco.sanhueza.cid@gmail.com>
2021-10-01 15:57:31 -0300

    entrada relacionada

--- src/Modelo/Entrada.java
+++ src/Modelo/Entrada.java
@@ -3,14 +3,18 @@ package Modelo;
 public class Entrada {
 	private int precio;
 	private String asiento;
+	private Cliente cliente;
+	private Vendedor vendedor;
 	
 	public Entrada() {
 		
 	}
-	public Entrada(int precio, String asiento) {
-		super();
+	public Entrada(int precio, String asiento, Cliente cliente, Vendedor vendedor) {
+		//super();
 		this.precio = precio;
 		this.asiento = asiento;
+		this.cliente = cliente;
+		this.vendedor = vendedor;
 	}
 	
 	public int getPrecio() {
@@ -24,6 +28,23 @@ public class Entrada {
 	}
 	public void setAsiento(String asiento) {
 		this.asiento = asiento;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	@Override
+	public String toString() {
+		return "Entrada [precio=" + precio + ", asiento=" + asiento + ", cliente=" + cliente + ", vendedor=" + vendedor
+				+ "]";
 	}
 	
 	
