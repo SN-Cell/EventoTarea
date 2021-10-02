867b6d09d104ab26c8fb092ebc2d15d815359630
Francisco Sanhueza <francisco.sanhueza.cid@gmail.com>
2021-10-02 09:23:31 -0300

    agregar clientes y vendedores

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -3,6 +3,8 @@ package MainPackage;
 import java.util.Scanner;
 
 import Modelo.Evento;
+import Modelo.Cliente;
+import Modelo.Vendedor;
 
 public class MainClass {
 
@@ -30,6 +32,48 @@ public class MainClass {
 		for(int i=0;i<ev.length;i++) {
 			System.out.println(ev[i].getNombreEvento()+" "+ev[i].getEdadMinima()+" ");
 		}
+		
+		String rutVendedor = "";
+		String nombreVendedor = "";
+		String fechaNacimientoVen = "";
+		Integer EntradasVendidasVen = 0; 
+		
+		Vendedor ven[] = new Vendedor[2];
+		
+		for(int i=0;i<ev.length;i++) {
+			System.out.println("Ingresa el Rut del Vendedor");
+			rutVendedor = sc.nextLine();
+			System.out.println("Ingrese el Nombre del Vendedor");
+			nombreVendedor = sc.nextLine();
+			System.out.println("Ingrese el Fecha de Nacimiento del Vendedor");
+			fechaNacimientoVen = sc.nextLine();
+			sc.nextLine();
+			
+			ven[i] = new Vendedor(rutVendedor,nombreVendedor,fechaNacimientoVen,EntradasVendidasVen);	
+		}
+		
+		
+		String rutCliente = "";
+		String nombreCliente = "";
+		String fechaNacimientoCl = "";
+		
+		Cliente cl[] = new Cliente[2];
+		
+		for(int i=0;i<ev.length;i++) {
+			System.out.println("Ingresa el Rut del Cliente");
+			rutCliente = sc.nextLine();
+			System.out.println("Ingrese el Nombre del Cliente");
+			nombreCliente = sc.nextLine();
+			System.out.println("Ingrese el Fecha de Nacimiento del Cliente");
+			fechaNacimientoCl = sc.nextLine();
+			sc.nextLine();
+			
+			cl[i] = new Cliente(rutCliente,nombreCliente,fechaNacimientoCl);	
+		}
+		
+		
+		
+		
 		
 		
 		//System.out.println(ev[0].getNombreEvento());
